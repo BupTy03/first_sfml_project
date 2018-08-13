769bf74e7a95442046c69183577802f269c8e1b7
BupTy03 <41854435+BupTy03@users.noreply.github.com>
2018-08-13 12:31:48 +0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -12,6 +12,7 @@
 using std::enable_if_t;
 using std::is_floating_point_v;
 
+using sf::ContextSettings;
 using sf::RenderWindow;
 using sf::Event;
 using sf::VideoMode;
@@ -32,74 +33,81 @@ constexpr inline T to_radian(T deg) noexcept
 
 void update_my_window(Clock& t, const RenderWindow& window, CircleShape& circle, int step, int& angle)
 {
+	static int downsize = 1;		// шаг увеличения / уменьшения окружности 
+	constexpr float min_size = 10;	// минимальный размер окружности
+	constexpr float max_size = 150;	// максимальный размер окружности
+
+	static float radius = circle.getRadius();	// радиус окружности
+
+	if ((t.getElapsedTime()).asSeconds() < 0.005f) {
+		circle.setRadius(radius);
+		return;
+	}
+
 	angle %= 360;	// чтобы отсечь лишнее(если угол больше 360)
 
-	static int count = 0;
-	static bool downsize = false;	// окружность уменьшается / увеличивается
-	constexpr float min_size = 10;	// минимальный размер окружности
-	constexpr float max_size = 500;	// максимальный размер окружности
+	float pos_x = circle.getPosition().x + step * cos(to_radian(static_cast<float>(angle)));
+	float pos_y = circle.getPosition().y + step * sin(to_radian(static_cast<float>(angle)));
 
-	float radius = circle.getRadius();	// радиус окружности
+	const auto win_sz_x = static_cast<float>(window.getSize().x);
+	const auto win_sz_y = static_cast<float>(window.getSize().y);
 
-	if ((t.getElapsedTime()).asSeconds() >= 0.005f)
+	bool angle_changed = false;
+
+	if (pos_x < 0.f || pos_x + circle.getRadius() * 2.f >= win_sz_x)
 	{
-		if (++count >= 30)
-		{
-			radius += (downsize) ? -1 : 1;
-
-			if (radius >= max_size){
-				downsize = true;
-			}
-			else if (radius <= min_size){
-				downsize = false;
-			}
-			circle.setRadius(radius);
-			count = 0;
-		}
-
-		float pos_x = circle.getPosition().x + step * cos(to_radian(static_cast<float>(angle)));
-		float pos_y = circle.getPosition().y + step * sin(to_radian(static_cast<float>(angle)));
-
-		const auto win_sz_x = static_cast<float>(window.getSize().x);
-		const auto win_sz_y = static_cast<float>(window.getSize().y);
-
-		if (pos_x < 0.f || pos_x + radius * 2.f >= win_sz_x)
-		{
-			angle = 180 - angle;
-			circle.setFillColor(Color(rand_val(0, 255), rand_val(0, 255), rand_val(0, 255)));
-		}
-
-		if (pos_y < 0.f)
-		{
-			if (angle == 270 || angle == -90) {
-				angle = 90;
-			}
-			else {
-					angle = angle * 2 + 90;
-			}
-			circle.setFillColor(Color(rand_val(0, 255), rand_val(0, 255), rand_val(0, 255)));
-		}
-		else if (pos_y + radius * 2.f >= win_sz_y)
-		{
-			if (angle == 90 || angle == -270)
-				angle = 270;
-			else {
-				angle = angle * 2 - 90;
-			}
-			circle.setFillColor(Color(rand_val(0, 255), rand_val(0, 255), rand_val(0, 255)));
-		}
-
-		pos_x = circle.getPosition().x + step * cos(to_radian(static_cast<float>(angle)));
-		pos_y = circle.getPosition().y + step * sin(to_radian(static_cast<float>(angle)));
-
-		circle.setPosition(pos_x, pos_y);
-		t.restart();
+		angle = 180 - angle;
+		angle_changed = true;
 	}
+
+	if (pos_y < 0.f)
+	{
+		if (angle == 270) {
+			angle = 90;
+		}
+		else {
+			angle = angle * 2 + 90;
+		}
+		angle_changed = true;
+	}
+	else if (pos_y + circle.getRadius() * 2.f >= win_sz_y)
+	{
+		if (angle == 90) {
+			angle = 270;
+		}
+		else {
+			angle = angle * 2 - 90;
+		}
+		angle_changed = true;
+	}
+
+	if (angle_changed)
+	{
+		if (angle < 0) {
+			angle += 360;
+		}
+
+		circle.setFillColor(Color(rand_val(0, 255), rand_val(0, 255), rand_val(0, 255)));
+
+		if (radius >= max_size || radius <= min_size) {
+			downsize *= -1;
+		}
+		radius += downsize;
+	}
+
+	pos_x = circle.getPosition().x + step * cos(to_radian(static_cast<float>(angle)));
+	pos_y = circle.getPosition().y + step * sin(to_radian(static_cast<float>(angle)));
+
+	circle.setPosition(pos_x, pos_y);
+	t.restart();
 }
 
 int main()
 {
-	RenderWindow window(VideoMode(1920, 1080), "My Window", sf::Style::Fullscreen);
+	ContextSettings settings;
+	settings.antialiasingLevel = 8;	// сглаживание x8
+
+	RenderWindow window(VideoMode(800, 600), "My Window", sf::Style::Default, settings);
 
 	CircleShape csh(50.f);
 	csh.setPosition(window.getSize().x / 2.f - csh.getRadius(), window.getSize().y / 2.f - csh.getRadius());
